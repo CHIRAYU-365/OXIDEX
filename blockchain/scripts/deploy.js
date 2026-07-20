97555a3719038eeabca499c3d57f6391b9162c55
chirayu <chirayujain362005@gmail.com>
2026-07-20 14:04:35 +0530

    Deploy VIP NFT Gallery and Enterprise Reentrancy Security

--- blockchain/scripts/deploy.js
+++ blockchain/scripts/deploy.js
@@ -19,6 +19,13 @@ async function main() {
   const tokenAddress = await token.getAddress();
   console.log("OxiToken deployed to:", tokenAddress);
 
+  // Deploy NFT VIP Pass Contract
+  const OxideXNFT = await hre.ethers.getContractFactory("OxideXNFT");
+  const nft = await OxideXNFT.deploy(tokenAddress);
+  await nft.waitForDeployment();
+  const nftAddress = await nft.getAddress();
+  console.log("OxideXNFT deployed to:", nftAddress);
+
   // Link Contracts
   console.log("Linking contracts...");
   let tx;
@@ -30,6 +37,10 @@ async function main() {
   tx = await token.setLaunchpadContract(oxideXAddress);
   await tx.wait(1);
   console.log("- OxiToken linked to OxideXBase");
+
+  tx = await oxideX.setNFTContract(nftAddress);
+  await tx.wait(1);
+  console.log("- OxideXBase linked to OxideXNFT");
 
   console.log("All contracts successfully deployed and linked!");
 }
